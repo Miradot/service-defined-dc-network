7a790f120610b4e12692e796ed19547dde2bd811
Johan Ekström <johan.ekstrom@miradot.se>
2022-08-31 15:09:02 +0200

    some doc

--- modules/services/variables.tf
+++ modules/services/variables.tf
@@ -1,15 +1,17 @@
 variable "customer" {
   type = string
-  description = "The name of the customer for the service"
+  description = "The name of the customer that will be deployed"
 }
 
 variable "customer_services" {
   type    = set(string)
   default = []
+  description = "Sets of service_ids that will be deployed under this customer"
 }
 
 variable "supported_service_types" {
   type = list(string)
+  description = "List of the current supported service types. If develop more service types, it will have to be added to the list when it services should be deployed."
 }
 
 variable "services" {
@@ -25,9 +27,11 @@ variable "services" {
       vlan = optional(number)
     })
   }))
+  description = "The actual services definitions. Depending on what type of service, different paramters are required."
 }
 
 variable "physical_domain" {
   type = string
   default = "phys"
+  description = "The physical domain that should be mapped to under each EPG that is a L2VPN"
 }
